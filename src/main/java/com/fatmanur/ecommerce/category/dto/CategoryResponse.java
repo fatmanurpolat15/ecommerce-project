ff0d2ec8ca509e0ef620dc8617453f4816e32189
fatmanurpolat <ftmnrplt15@gmail.com>
2026-08-14 23:54:50 +0300

    BR-PRD-6

--- src/main/java/com/fatmanur/ecommerce/category/dto/CategoryResponse.java
+++ src/main/java/com/fatmanur/ecommerce/category/dto/CategoryResponse.java
@@ -1,7 +1,13 @@
 package com.fatmanur.ecommerce.category.dto;
 
+import java.io.Serializable;
+
 public record CategoryResponse(
         Long id,
         String name,
         String description
-) {}
+
+)  implements Serializable{}
+
+
+
